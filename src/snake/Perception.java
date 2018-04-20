e3dc5fd2846045e5851f4cf176e08d2350070f64
Tomás Avila Welling <2161611@my.ipleiria.pt>
2018-04-20 11:38:10 +0100

    Base del proyecto IA.

--- src/snake/Perception.java
+++ src/snake/Perception.java
@@ -1,5 +1,34 @@
 package snake;
 
 public class Perception {
-    // TODO
+    private Cell n, s, e, w;
+    private Food food;
+
+    public Perception(Cell n, Cell s, Cell e, Cell w, Food food) {
+        this.n = n;
+        this.s = s;
+        this.e = e;
+        this.w = w;
+        this.food = food;
+    }
+
+    public Cell getN() {
+        return n;
+    }
+
+    public Cell getS() {
+        return s;
+    }
+
+    public Cell getE() {
+        return e;
+    }
+
+    public Cell getW() {
+        return w;
+    }
+
+    public Food getFood() {
+        return food;
+    }
 }
